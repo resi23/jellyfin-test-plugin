fa0b04e37c78bda5cb8b239ea48262512abc6702
resi23 <x-resistant-x@gmx.de>
2024-05-17 17:55:29 +0200

    Init Commit

--- Jellyfin.Plugin.Template/Plugin.cs
+++ Jellyfin.Plugin.Template/Plugin.cs
@@ -26,10 +26,10 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     }
 
     /// <inheritdoc />
-    public override string Name => "Template";
+    public override string Name => "Deine Mudda";
 
     /// <inheritdoc />
-    public override Guid Id => Guid.Parse("eb5d7894-8eef-4b36-aa6f-5d124e828ce1");
+    public override Guid Id => Guid.Parse("4fabd0f7-1bc8-4983-bc8c-9f6cdd695ca4");
 
     /// <summary>
     /// Gets the current plugin instance.
